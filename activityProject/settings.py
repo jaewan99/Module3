ccfba75b02f89f9c10809872b44449ec8546980d
jaewan99 <antjw1999@gmail.com>
2024-03-31 21:16:02 +0800

    'debugValue'

--- activityProject/settings.py
+++ activityProject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = ('True')
+DEBUG = os.environ['DEBUG_VALUE'] == 'TRUE'
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost', 'djangoposdb-08ee64e241fb.herokuapp.com']
 
