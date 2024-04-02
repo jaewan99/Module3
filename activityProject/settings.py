f550863388358581f675e7c57cf0556d21bd6bca
jaewan99 <antjw1999@gmail.com>
2024-04-02 20:28:09 +0800

    'csrf_token'

--- activityProject/settings.py
+++ activityProject/settings.py
@@ -33,8 +33,11 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost', 'djangoposdb-08ee64e241fb.herokuapp.com']
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r'^https://posdb\.joji\.tech.*$',
+CORS_ALLOWED_ORIGINS = [
+"https://posdb.joji.tech",
+"https://djangoposdb-08ee64e241fb.herokuapp.com/",
+"http://localhost:8080",
+"http://127.0.0.1:9000"
 ]
 # Application definition
 
